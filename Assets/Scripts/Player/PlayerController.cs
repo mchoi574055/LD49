d79f00faac1cd57abb8ade8ecd0ab9a2847b32a9
Daniel Tsai <32005886+danieltsai-RSheepDV@users.noreply.github.com>
2021-10-02 06:07:08 -0700

    Added controllable player and basic collision

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float accelerationSpeed = 4f;
+
+    private Vector3 direction;
+    private Rigidbody2D rb;
+
+    private Item currentItem;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        direction = Vector3.zero;
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        currentItem = Item.NONE;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.velocity = Vector3.Lerp(rb.velocity, direction * moveSpeed, Time.deltaTime * accelerationSpeed);
+    }
+
+    //Events
+    public void OnMove(InputValue movementValue)
+    {
+        Debug.Log(movementValue.Get<Vector2>());
+        direction = new Vector3(movementValue.Get<Vector2>().x, movementValue.Get<Vector2>().y, 0);
+    }
+    
+    //Getters and Setters
+    public Item getCurrentItem()
+    {
+        return currentItem;
+    }
+
+    public void setCurrentItem(Item i)
+    {
+        currentItem = i;
+    }
+}
