3beb30c8922c41d0d3c1f3fd3ebbc89b5169c5e5
ethan-chan2 <62733311+ethan-chan2@users.noreply.github.com>
2021-10-02 22:04:49 -0700

    Progress on door

---
+++ Assets/Scripts/Door/Door.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+   /* private void open()
+    {
+        gameObject.SetActive(false); //Door opens, so it's deleted.
+    }
+   */
+    public void OnTriggerEnter2D(Collision2D collider)
+    {
+        Debug.Log("Hi");
+       // player = collider.gameObject.GetComponent<PlayerController>();
+        if (collider.gameObject == player)
+        {
+            
+          //  if (player.getCurrentItem() == Item.KEY) // If the player near the door is holding a key, open.
+           // {
+                gameObject.SetActive(false);
+          //  }
+        }
+    }
+}
