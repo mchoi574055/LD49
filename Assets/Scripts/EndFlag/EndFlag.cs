6fd0d3e6952dfc286e75bbcadca0d483c3d7917b
Daniel Tsai <32005886+danieltsai-RSheepDV@users.noreply.github.com>
2021-10-04 03:26:36 -0700

    Iteration1 Completed

--- Assets/Scripts/EndFlag/EndFlag.cs
+++ Assets/Scripts/EndFlag/EndFlag.cs
@@ -1,18 +1,17 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EndFlag : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject interactor;
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (other.gameObject == interactor)
+        {
+            transform.Find("Canvas").gameObject.SetActive(true);
+        }
     }
 }
