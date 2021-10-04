6c3b7365b2dcd6f43c7ee946faa96c07770bf7ea
Justine Pendergraft <justinetulip@gmail.com>
2021-10-03 18:00:35 -0700

    Quick commit before pull

--- Assets/Scripts/Button/TestButton.cs
+++ Assets/Scripts/Button/TestButton.cs
@@ -17,13 +17,14 @@ public class TestButton : MonoBehaviour
 
     public void press()
     {
+        Debug.Log("press")
         buttonPressed = true;
-
         spRenderer.color = new Color(0f, 0f, 0f, 1f); // Set to opaque black
     }                                                                 
     
     public void release()
     {
+        Debug.Log("press")
         buttonPressed = false;
         spRenderer.color = new Color(255f, 100f, 100f, 1f);
     }  
